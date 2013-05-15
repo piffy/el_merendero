7beb6e4894c5edc8e7170b296ba66a2240264a29
Bravo <bravogit@gmail.com>
2013-05-15 18:01:38 +0200

    Rifatto i test con Ordine.

--- El_Merendero/test/el_merendero/OrdineDiClasseTest.java
+++ El_Merendero/test/el_merendero/OrdineDiClasseTest.java
@@ -18,27 +18,25 @@ public class OrdineDiClasseTest {
      */
     @Test
     public void testCostructor() {
-        try{
-        OrdineDiClasse odc = new OrdineDiClasse();
-        }catch(Exception e){
-            assertEquals("Test per il costruttore.","L'ordine deve contenere il nome della classe.",e.getMessage());
+        try {
+            OrdineDiClasse odc = new OrdineDiClasse();
+        } catch (Exception e) {
+            assertEquals("Test per il costruttore.", "L'ordine deve contenere il nome della classe.", e.getMessage());
         }
     }
-    
+
     /**
      * Test of getTotale method, of class OrdineDiClasse.
      */
     @Test
     public void testGetTotale() {
         OrdineDiClasse odc = new OrdineDiClasse("4B Info");
-        Merenda m = new Merenda(4.3f, "Panino 1");
-        Listamerende lm = new Listamerende(m);
+        Merenda m = new Merenda("Panino 1", 4.3f);
         Ordine o = new Ordine("Manfredini");
-        o.setMerendeOrdinate(lm);
+        o.add(m);
         odc.add(o);
-        lm = new Listamerende(new Merenda(5.6f, "Panino 2"));
         o = new Ordine("Eddy");
-        o.setMerendeOrdinate(lm);
+        o.add(new Merenda("Panino 2", 5.6f));
         odc.add(o);
         Float expected = 9.9f;
         assertEquals("Test per il totale dell'ordine", expected, odc.getTotale(), 0.001);
@@ -50,16 +48,16 @@ public class OrdineDiClasseTest {
     @Test
     public void testToString() {
         OrdineDiClasse odc = new OrdineDiClasse("4B Info");
-        Merenda m = new Merenda(4.3f, "Panino 1");
-        Listamerende lm = new Listamerende(m);
+        Merenda m = new Merenda("Panino 1", 4.3f);
         Ordine o = new Ordine("Manfredini");
-        o.setMerendeOrdinate(lm);
+        o.add(m);
         odc.add(o);
-        lm = new Listamerende(new Merenda(5.6f, "Panino 2"));
         o = new Ordine("Eddy");
-        o.setMerendeOrdinate(lm);
+        m = new Merenda("Panino 2", 5.6f);
+        o.add(m);
         odc.add(o);
-        String expected = "4B Info\nOrdine: Manfredini\nNome = Panino 1  Prezzo = 4.3\n\nOrdine: Eddy\nNome = Panino 2  Prezzo = 5.6\n\n";
+        System.out.println(odc.toString());
+        String expected = "4B Info\nOrdine: Manfredini\n1x Panino 1 - 4.3€\n\nOrdine: Eddy\n1x Panino 2 - 5.6€\n\n";
         assertEquals("Test per l'ordine di classe", expected, odc.toString());
     }
 }
