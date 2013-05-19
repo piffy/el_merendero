bfcea1fae1133e6e24a2477287a5170911240b7c
Bravo <bravogit@gmail.com>
2013-05-19 09:16:27 +0200

    Aggiunto il ActionListener di cmdFatto

--- El_Merendero/src/el_merendero/FrameClasseStudente.java
+++ El_Merendero/src/el_merendero/FrameClasseStudente.java
@@ -9,6 +9,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.io.FileNotFoundException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.*;
 
 /**
@@ -16,7 +19,7 @@ import javax.swing.*;
  * @author charlie
  */
 public class FrameClasseStudente extends JFrame {
-    
+
     private JComboBox boxClassi;
     private JComboBox boxStudenti;
     private String nomiClassi[];
@@ -28,72 +31,90 @@ public class FrameClasseStudente extends JFrame {
     private JPanel Studenti = new JPanel();
     private String Classe;
     private String Studente;
-    
-    public FrameClasseStudente(){
+
+    public FrameClasseStudente() {
         super("El_Merendero");
-        
+
         setLayout(new BorderLayout());
-        
-        cmdFatto=new JButton("Fatto");
-        cmdIndietro=new JButton("Indietro");
-        
+
+        cmdFatto = new JButton("Fatto");
+        cmdIndietro = new JButton("Indietro");
+
         list = new ListaClassiHardwired();
-        
-        nomiClassi=list.getNomiClassi();
-        
+
+        nomiClassi = list.getNomiClassi();
+
         nomiStudenti = list.ListaStudentiXClasseData(nomiClassi[0]);
         Classe = nomiClassi[0];
         boxStudenti = new JComboBox(nomiStudenti); //set up JComboBox
-        boxClassi = new JComboBox( nomiClassi ); // set up JComboBox
-        boxClassi.setMaximumRowCount( 10 );
-        boxStudenti.setMaximumRowCount( 15 );
+        boxClassi = new JComboBox(nomiClassi); // set up JComboBox
+        boxClassi.setMaximumRowCount(10);
+        boxStudenti.setMaximumRowCount(15);
         Classi.add(boxClassi);
-        add(Classi,BorderLayout.NORTH);
+        add(Classi, BorderLayout.NORTH);
         Studenti.add(boxStudenti);
-        add(Studenti,BorderLayout.CENTER);
+        add(Studenti, BorderLayout.CENTER);
         boxClassi.addItemListener(new ItemListener() {
             // handle JComboBox event
             @Override
-            public void itemStateChanged( ItemEvent e )
-            {
-               // determine whether checkbox selected
-               if ( e.getStateChange() == ItemEvent.SELECTED ){
-                   nomiStudenti = list.ListaStudentiXClasseData(e.getItem().toString());
-                   Classe=e.getItem().toString();
-                   boxStudenti.removeAllItems();
-                   for(int i=0;i<nomiStudenti.length;i++) {
-                       boxStudenti.addItem(nomiStudenti[i]);
-                   }
-                   Studenti.add(boxStudenti);
-                   add(Studenti,BorderLayout.CENTER);
-               }
+            public void itemStateChanged(ItemEvent e) {
+                // determine whether checkbox selected
+                if (e.getStateChange() == ItemEvent.SELECTED) {
+                    nomiStudenti = list.ListaStudentiXClasseData(e.getItem().toString());
+                    Classe = e.getItem().toString();
+                    boxStudenti.removeAllItems();
+                    for (int i = 0; i < nomiStudenti.length; i++) {
+                        boxStudenti.addItem(nomiStudenti[i]);
+                    }
+                    Studenti.add(boxStudenti);
+                    add(Studenti, BorderLayout.CENTER);
+                }
             } // end method itemStateChanged
-         } // end anonymous inner class
-      ); // end call to addItemListener
-        boxStudenti.addItemListener(new ItemListener(){
-         @Override
-         public void itemStateChanged( ItemEvent e ) {
-             if(e.getStateChange() == ItemEvent.SELECTED)
-                 Studente = e.getItem().toString();
-         }
+        } // end anonymous inner class
+                ); // end call to addItemListener
+        boxStudenti.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                if (e.getStateChange() == ItemEvent.SELECTED) {
+                    Studente = e.getItem().toString();
+                }
+            }
         });
         Box horizontal = Box.createHorizontalBox();
         horizontal.add(cmdIndietro);
         horizontal.add(Box.createHorizontalGlue());
         horizontal.add(Box.createRigidArea(new Dimension(115, 8)));
         horizontal.add(cmdFatto);
-        add(horizontal,BorderLayout.SOUTH);
+        add(horizontal, BorderLayout.SOUTH);
         cmdIndietro.addActionListener(
-                new ActionListener(){
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                    FrameClasseStudente.this.setVisible(false);
-                    Frame fb = new Frame();
-                    fb.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-                    fb.setSize( 800, 600 ); // set frame size
-                    fb.setLocationRelativeTo(null); //center fr
-                    fb.setVisible( true ); // display frame	
-                }
-            });
+                new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        FrameClasseStudente.this.setVisible(false);
+                        Frame fb = new Frame();
+                        fb.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                        fb.setSize(800, 600); // set frame size
+                        fb.setLocationRelativeTo(null); //center fr
+                        fb.setVisible(true); // display frame	
+                    }
+                });
+        cmdFatto.addActionListener(
+                new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        try {
+                            FrameClasseStudente.this.setVisible(false);
+                            OrdineDiClasse odc = new OrdineDiClasse(Classe);
+                            odc.add(new Ordine(Studente));
+                            FrameOrdine fb = new FrameOrdine(odc);
+                            fb.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                            fb.setSize(800, 600); // set frame size
+                            fb.setLocationRelativeTo(null); // display frame	
+                            fb.setVisible(true); // display frame
+                        } catch (FileNotFoundException ex) {
+                            Logger.getLogger(FrameClasseStudente.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }
+                });
     }
 }
